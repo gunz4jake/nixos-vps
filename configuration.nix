f90f561965a3dd985f7d43d11ab428b500d61cab
Jacob Karr <jacob@ningen.gg>
2026-02-17 00:59:39 +0000

    added htop

--- configuration.nix
+++ configuration.nix
@@ -81,6 +81,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     wget
+    htop
   ];
 
   # Open ports in the firewall.
